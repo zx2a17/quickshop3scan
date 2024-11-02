855f04143d43cb5df8b987a6d4f8b65af1c88c33
Alex Bunting <alex.bunting314@gmail.com>
2024-11-02 23:57:04 +1100

    Initial commit with app icon

---
+++ android/app/src/main/kotlin/com/buntagon/quickshop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.buntagon.quickshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+import 'src/app.dart';
+import 'src/settings/settings_controller.dart';
+import 'src/settings/settings_service.dart';
+
+void main() async {
+  timeDilation = 5.0; // 1.0 is normal animation speed.
+
+  // Set up the SettingsController, which will glue user settings to multiple
+  // Flutter Widgets.
+  final settingsController = SettingsController(SettingsService());
+
+  // Load the user's preferred theme while the splash screen is displayed.
+  // This prevents a sudden theme change when the app is first displayed.
+  await settingsController.loadSettings();
+
+  // Run the app and pass in the SettingsController. The app listens to the
+  // SettingsController for changes, then passes it further down to the
+  // SettingsView.
+  runApp(MyApp(settingsController: settingsController));
+}
